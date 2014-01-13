13e575f1aceae058e305dffb4b7b9620d458d128
Guz Alexander <kalimatas@gmail.com>
2014-01-13 23:01:03 +0400

    Store Vertex in array

--- src/github.com/kalimatas/graph/graph.go
+++ src/github.com/kalimatas/graph/graph.go
@@ -1,14 +1,10 @@
 package graph
 
-import (
-	"github.com/kalimatas/structures"
-)
-
 const MAX_VERTS uint = 20
 
 type Graph struct {
-	VertexList  structures.List
-	AdjMatrix   [MAX_VERTS][MAX_VERTS]bool // adjacency matrix
+	vertexList  [MAX_VERTS]*Vertex
+	adjMatrix   [MAX_VERTS][MAX_VERTS]bool // adjacency matrix
 	VertexCount uint                       // current number of verteces
 }
 
@@ -20,24 +16,27 @@ func (e *OutOfBoundariesError) Error() string {
 	return e.message
 }
 
-func (graph *Graph) addVertex(vertex *Vertex) (err error) {
+func (graph *Graph) AddVertex(vertex *Vertex) (err error) {
 	if graph.VertexCount >= MAX_VERTS {
 		err = &OutOfBoundariesError{"Reached max verteces count"}
 	}
 
-	graph.VertexList.InsertFirst(vertex)
 	graph.VertexCount++
-
+	graph.vertexList[graph.VertexCount] = vertex
 	return
 }
 
-func (graph *Graph) addEdge(start, end uint) (err error) {
+func (graph *Graph) AddEdge(start, end uint) (err error) {
 	if start >= MAX_VERTS || end >= MAX_VERTS {
 		err = &OutOfBoundariesError{"Vertex is out of bound"}
 	}
 
-	graph.AdjMatrix[start][end] = true
-	graph.AdjMatrix[end][start] = true
+	graph.adjMatrix[start][end] = true
+	graph.adjMatrix[end][start] = true
 
 	return
+}
+
+func (graph *Graph) GetVertextCount() (count uint) {
+	return graph.VertexCount
 }
