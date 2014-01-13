15f4828c36c95a857260cebe59de775183f45f12
Guz Alexander <kalimatas@gmail.com>
2014-01-13 22:23:58 +0400

    Make list double linked

--- src/github.com/kalimatas/structures/list.go
+++ src/github.com/kalimatas/structures/list.go
@@ -15,17 +15,47 @@ func (n *node) String() string {
 // List data structure.
 type List struct {
 	first *node
+	last  *node
+}
+
+func (list *List) IsEmpty() bool {
+	return list.first == nil
 }
 
 // InsertFirst inserts a node in the beginning of a list.
 func (list *List) InsertFirst(value interface{}) {
-	newNode := node{value, list.first}
+	newNode := node{value: value}
+	if list.IsEmpty() {
+		list.last = &newNode
+	}
+
+	newNode.next = list.first
 	list.first = &newNode
+}
+
+// InsertLast inserts a node to the end of a list.
+func (list *List) InsertLast(value interface{}) {
+	newNode := node{value: value}
+	if list.IsEmpty() {
+		list.first = &newNode
+	} else {
+		list.last.next = &newNode
+	}
+	
+	list.last = &newNode
 }
 
 // DeleteFirst removes and returns the first node of a list.
 func (list *List) DeleteFirst() (value interface{}) {
+	if list.IsEmpty() {
+		return
+	}
+
 	value = list.first.value
+	if list.first.next == nil {
+		list.last = nil
+	}
+	
 	list.first = list.first.next
 	return
 }
@@ -34,5 +64,5 @@ func (list *List) DeleteFirst() (value interface{}) {
 func (list *List) Display() {
 	for current := list.first; current != nil; current = current.next {
 		fmt.Printf("%s ", current)
-	} 
+	}
 }
